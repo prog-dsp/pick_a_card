fca3f655eb06a628eeace756d3a02edf32db89eb
prog_dsp <prog_dsp@mail.ru>
2020-10-15 15:53:10 +0300

    change for UNICODE characters

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"] # Буби, Черви, Крести, Пики
+suits = ["\u2666", "\u2665", "\u2663", "\u2660"] # Буби, Черви, Крести, Пики
 
 puts "#{values.sample} of #{suits.sample}"
